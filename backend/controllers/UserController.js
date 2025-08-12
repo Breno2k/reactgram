669755d6f5d160312f172df989ad27d23afc895d
Breno Melo <brenolucasmelo@gmail.com>
2025-08-12 15:19:05 -0300

    Criando registro do usuário

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,20 +1,51 @@
 const User = require("../models/User")
 
-const bcript = require("bcryptjs")
+const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 
 const jwtSecret = process.env.JWT_SECRET
 
 // generate user token
 const generateToken = (id) => {
-    return jwt.sign({ id }, JWT_SECRET, {
+    return jwt.sign({ id }, jwtSecret, {
         expiresIn: "7d",
     })
 }
 
 // register user and sign in
 const register = async (req, res) => {
-    res.send("Registro");
+
+    const { name, email, password } = req.body
+
+    // check if user exists
+    const user = await User.findOne({ email })
+
+    if (user) {
+        res.status(422).json({ errors: ["Por favor, utilize outro email."] })
+        return
+    }
+
+    // Generate password hash
+    const salt = await bcrypt.genSalt()
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    // Create user
+    const newUser = await User.create({
+        name,
+        email,
+        password: passwordHash
+    })
+
+    // If user was created successfully, return the token
+    if (!newUser) {
+        res.status(422).json({ errors: ["Ouve um erro, por favor tente mais tarde."] })
+        return
+    }
+
+    res.status(201).json({
+        _id: newUser._id,
+        token: generateToken(newUser._id),
+    })
 };
 
 module.exports = {
