dd8940d17ae177a0e35910d6274e4aef843a9f25
Breno Melo <brenolucasmelo@gmail.com>
2025-08-12 14:47:00 -0300

    Implementando back-end

---
+++ backend/config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+
+const conn = async () => {
+
+    try {
+
+        const dbConn = await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.lgjowxz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+
+
+        console.log("Conectou ao Banco!")
+
+        return dbConn
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+conn()
+
+module.exports = conn
