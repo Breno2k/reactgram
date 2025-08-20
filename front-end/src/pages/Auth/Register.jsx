0c8b3369727c9528d556dd01c030275a21d687cc
Breno Melo <brenolucasmelo@gmail.com>
2025-08-20 12:05:13 -0300

    Criando states do form de cadastro

--- front-end/src/pages/Auth/Register.jsx
+++ front-end/src/pages/Auth/Register.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import "./Auth.css"
 
 // Componentes
@@ -8,8 +9,26 @@ import { Link } from 'react-router-dom'
 
 const Register = () => {
 
+    // Criando os states de registro
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+
+
+    // função de envio de formulário
     const handleSubmit = (e) => {
         e.preventDefault()
+
+        // Aqui se cria um usuário com esses dados
+        const user = {
+            name,
+            email,
+            password,
+            confirmPassword
+        }
+
+        console.log(user)
     }
 
 
@@ -17,10 +36,10 @@ const Register = () => {
         <h2>Reactgram</h2>
         <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
         <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="Nome" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Senha" />
-            <input type="password" placeholder="Confirme sua senha" />
+            <input type="text" placeholder="Nome" onChange={(e) => setName(e.target.value)} value={name} />
+            <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} value={email} />
+            <input type="password" placeholder="Senha" onChange={(e) => setPassword(e.target.value)} value={password} />
+            <input type="password" placeholder="Confirme sua senha" onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword} />
             <input type="submit" value="Cadastrar" />
         </form>
         <p>Já tem conta ? <Link to="/login">Click aqui</Link></p>
