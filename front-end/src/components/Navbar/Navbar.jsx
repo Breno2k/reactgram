df2c61404cd36fcc464ecbc7ea36d1705c2aa8a9
Breno Melo <brenolucasmelo@gmail.com>
2025-08-21 16:22:56 -0300

    função de logout implementada na navbar

--- front-end/src/components/Navbar/Navbar.jsx
+++ front-end/src/components/Navbar/Navbar.jsx
@@ -10,11 +10,24 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { useAuth } from '../../hooks/useAuth'
 
+// Redux
+import { logout, reset } from '../../slices/authSlice'
+
 const Navbar = () => {
 
     const { auth } = useAuth();
     const { user } = useSelector((state) => state.auth)
 
+    const navigate = useNavigate()
+
+    const dispatch = useDispatch()
+
+    const handleLogout = () => {
+        dispatch(logout())
+        dispatch(reset())
+
+        navigate("/login")
+    }
 
     return (
         <nav className={styles.nav}>
@@ -44,7 +57,7 @@ const Navbar = () => {
                             </NavLink>
                         </li>
                         <li>
-                            <span>Sair</span>
+                            <span onClick={handleLogout}>Sair</span>
                         </li>
                     </>
                 ) : (
