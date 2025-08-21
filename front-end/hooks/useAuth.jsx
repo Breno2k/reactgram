0bdf843a19c6f4aa01c5bcd3ed278449867dcff1
Breno Melo <brenolucasmelo@gmail.com>
2025-08-21 15:17:22 -0300

    Criando hook de verificação de autenticação

---
+++ front-end/hooks/useAuth.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
+
+export const useAuth = () => {
+
+    // Pega o usuário do Redux (state.auth.user)
+    const { user } = useSelector((state) => state.auth);
+
+    // Estado local para indicar se está autenticado
+    const [auth, setAuth] = useState(false);
+    // Estado local para indicar carregamento
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+
+        if (user) {
+            setAuth(true)
+        } else {
+            setAuth(false)
+        }
+
+        setLoading(false);
+
+    }, [user])
+
+    return { auth, loading }
+}
